966b58abcf716320eb164e747cff95016c6bef1e
Shwejan Kumar Enjamuri <shwejannn@gmail.com>
2025-08-19 20:48:54 +0530

    Added express middleware

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const dotenv = require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(express.json());
 app.use("/api/contacts", require("./routes/contact-routes"));
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`); 
 })  
