432c0fdec5109c0a6115af5315e931bd95405ec9
Shwejan Kumar Enjamuri <shwejannn@gmail.com>
2025-08-19 22:15:57 +0530

    Added db schema

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const errorHandler = require('./middleware/errorHandler');
+const connectDb = require('./config/dbConnection');
 const dotenv = require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 4000;
 
+connectDb();
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contact-routes"));
 app.use(errorHandler);
