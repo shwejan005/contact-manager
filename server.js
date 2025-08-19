5424446bdf530415235e9e7afd6ab336785c8a00
Shwejan Kumar Enjamuri <shwejannn@gmail.com>
2025-08-19 21:29:54 +0530

    Error handling middleware

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const errorHandler = require('./middleware/errorHandler');
 const dotenv = require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contact-routes"));
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`); 
-})  
+})
